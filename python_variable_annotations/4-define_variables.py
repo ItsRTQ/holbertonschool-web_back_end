717eccc6d6c0da1679ef870576ab6aeac6269527
William <titanbabrisa@gmail.com>
2024-04-23 16:54:31 -0400

    tasks

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python3
+"""This module defines 4 varibles a, pi, i_unde... , school"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
