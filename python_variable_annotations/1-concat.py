8f44602729879b08cecb9ac5082a173eb435fb78
William <titanbabrisa@gmail.com>
2024-04-23 16:18:32 -0400

    fix pycodestyle

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module defines the method concat"""
 
-def concat(str1 :str, str2 :str) -> str:
+
+def concat(str1: str, str2: str) -> str:
     """This method concat 2 strings into a new one"""
     return str1 + str2
