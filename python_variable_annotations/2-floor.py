49f6d34170f51e02808b01b129d3a861fe17275b
William <titanbabrisa@gmail.com>
2024-04-23 16:27:02 -0400

    task 3

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""This module deines the mehod floor"""
+"""This module deines the method floor"""
+
 
 def floor(n: float) -> int:
     """This method takes n and returns it as a int"""
--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""This module defines the method to_str"""
+
+
+def to_str(n: float) -> str:
+    """This method returns n as a str"""
+    return str(n)
