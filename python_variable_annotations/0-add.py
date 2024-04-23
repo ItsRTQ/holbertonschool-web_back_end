4d24a56ca129d75943cc796d5ed59abcca776ccd
William <titanbabrisa@gmail.com>
2024-04-23 16:12:01 -0400

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module takes defines the method add"""
+
+
+def add(a: float, b: float) -> float:
+    """This method take 2 num: float type and sums them"""
+
+    return a + b
