77e6f40e5ecaa7b18ad53daba066bc5f46dcd437
William <titanbabrisa@gmail.com>
2024-04-24 21:47:37 -0400

    changes

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """This module defines method sum_list"""
 
+
 def sum_list(input_list: list) -> float:
     """this method takes a list a sums them"""
+
     total: float = 0.0
     for i in input_list:
         total += i
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This module defines sum_mixed_list"""
+
+
+def sum_mixed_list(mxd_lst: list) -> float:
+    """This method takes a list with mix values and returns it total"""
+
+    total: float = 0.0
+    for i in mxd_lst:
+        total += i
+    return total
