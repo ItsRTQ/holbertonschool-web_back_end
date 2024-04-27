c3574d75b2cbcca3ad2cc69be28811ee2f4c1c3e
William <titanbabrisa@gmail.com>
2024-04-26 20:29:46 -0400

    changes

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python3
-"""This module defines the method task_wait_n"""
+"""This module defines wait_n"""
 import asyncio
+from typing import List
 
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n, max_delay):
-    """This method runs 2 process at the same time"""
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """This method creates a list of n elements using async"""
 
-    num_list: list = []
-    inserted = False
+    rand_list = []
+    temp = []
+
     for i in range(n):
-        rand = await task_wait_random(max_delay)
-        inserted = False
-        if not num_list:
-            num_list.append(rand)
-        else:
-            for indx, j in enumerate(num_list):
-                if rand < j:
-                    num_list.insert(indx, rand)
-                    inserted = True
-                    break
-            if not inserted:
-                num_list.append(rand)
-    return num_list
+        temp.append(asyncio.create_task(wait_random(max_delay)))
+    for number in asyncio.as_completed(temp):
+        rand_list.append(await number)
+
+    return sorted(rand_list)
