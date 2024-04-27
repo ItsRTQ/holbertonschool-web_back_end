71fc77d02cce85578c3f8ec0398bad5f5bd2e7e5
William <titanbabrisa@gmail.com>
2024-04-26 21:23:08 -0400

    changes

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""This module defines async_generator method"""
 import asyncio
 import random
 
+
 async def async_generator():
+    """This method generates random numbers from 0 - 10"""
     rand_list = []
     for i in range(10):
         random_num = random.randint(0, 10)
