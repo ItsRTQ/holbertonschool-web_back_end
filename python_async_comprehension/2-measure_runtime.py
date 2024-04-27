e17c9fd148699c3dc8a3e377f0e7f32b5c3fbcb1
William <titanbabrisa@gmail.com>
2024-04-26 22:39:20 -0400

    changes

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This module defines measure_runtime"""
+import asyncio
+from time import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    """This method measures the run time tasks completion"""
+
+    start = time()
+    tasks = [async_comprehension(), async_comprehension(),
+              async_comprehension(), async_comprehension()]
+    result = await asyncio.gather(*tasks)
+    end = time()
+    runTime = end - start
+    return runTime
