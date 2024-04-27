11d52e599b8960aff0d32294d03f8a4641b50392
William <titanbabrisa@gmail.com>
2024-04-26 21:48:52 -0400

    changes

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,5 +8,5 @@ async def async_generator():
     """This method generates random numbers from 0 - 10"""
 
     for _ in range(10):
-        await asyncio.sleep(1)
+        await asyncio.create_task(asyncio.sleep(1))
         yield random.randint(0, 10)
