40150fca357e57c92d71a83bd75b453aee37e5d2
William <titanbabrisa@gmail.com>
2024-04-26 20:04:00 -0400

    additions

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,19 +9,13 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> list:
     """This method creates a list of n elements using async"""
 
-    num_list: list = []
-    inserted = False
-    for i in range(n):
-        rand = await wait_random(max_delay)
-        inserted = False
-        if not num_list:
-            num_list.append(rand)
-        else:
-            for indx, j in enumerate(num_list):
-                if rand < j:
-                    num_list.insert(indx, rand)
-                    inserted = True
-                    break
-            if not inserted:
-                num_list.append(rand)
-    return num_list
+    delays = []
+
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    queue = asyncio.Queue()
+    for result in results:
+        await queue.put(result)
+    while not queue.empty():
+        delays.append(await queue.get())
+    return delays
