dfe078019ead67b501044f244eea4dc03e20ccab
William <titanbabrisa@gmail.com>
2024-04-26 22:22:20 -0400

    changes

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """This module defines async_comprehension method"""
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """This method collects 10 random numbers and return them"""
 
     result = []
