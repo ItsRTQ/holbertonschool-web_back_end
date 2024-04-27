cfe3364a688b8381a1b7369af1a8a5c287791c1f
William <titanbabrisa@gmail.com>
2024-04-26 20:25:09 -0400

    additions

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """This module defines wait_n"""
 import asyncio
-
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """This method creates a list of n elements using async"""
 
     rand_list = []
