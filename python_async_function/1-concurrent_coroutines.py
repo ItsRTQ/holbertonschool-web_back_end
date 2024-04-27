588a1a9dc087b8c9a796c65eea2e9a7cd3ffb241
William <titanbabrisa@gmail.com>
2024-04-26 20:15:14 -0400

    additions

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,14 +9,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """This method creates a list of n elements using async"""
 
-    num_list: list = []
-    inserted = False
-    for i in range(n):
-        rand: float = await wait_random(max_delay)
-        inserted = False
+    num_list = []
+
+    async def insert_sorted_list(rand):
+        """Helper for parent method"""
+        nonlocal num_list
         if not num_list:
             num_list.append(rand)
         else:
+            inserted = False
             for indx, j in enumerate(num_list):
                 if rand < j:
                     num_list.insert(indx, rand)
@@ -24,4 +25,8 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
                     break
             if not inserted:
                 num_list.append(rand)
+
+    tasks = [insert_sorted_list(await asyncio.create_task(wait_random(max_delay))) for _ in range(n)]
+    await asyncio.gather(*tasks)
+    
     return num_list
