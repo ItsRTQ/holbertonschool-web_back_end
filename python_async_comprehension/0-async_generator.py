2c8dbd2cefc9f5aae382c2f3c51af202ae2cece4
William <titanbabrisa@gmail.com>
2024-04-26 22:12:12 -0400

    changes

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> Generator[float, None, None]:
 
     for i in range(10):
         await asyncio.sleep(1)
-        yield 10 * random()
+        yield 10 * random.random()
