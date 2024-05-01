205572e69e64d457b3b74b2e98ef143fc2c8ebe6
William <titanbabrisa@gmail.com>
2024-05-01 18:28:38 -0400

    this propably not gonna work

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+import pymongo
+
+def get_nginx_logs_stats(mongo_uri):
+    client = pymongo.MongoClient(mongo_uri)
+    
+    db = client['logs']
+    collection = db['nginx']
+    
+    total_logs_count = collection.count_documents({})
+    
+    print(f"Total logs: {total_logs_count} logs")
+    
+    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: collection.count_documents({"method": method}) for method in http_methods}
+    
+    print("Methods:")
+    for method in http_methods:
+        print(f"\t{method}: {method_counts[method]}")
+    
+    status_logs_count = collection.count_documents({"method": "GET", "path": "/status"})
+    
+    print(f"Logs with method=GET and path=/status: {status_logs_count}")
+    client.close()
+
+
+if __name__ == "__main__":
+    mongo_uri = 'mongodb://localhost:27017'
+    get_nginx_logs_stats(mongo_uri)
