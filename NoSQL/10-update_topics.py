66e2a08d9a6d217b205c468c402f39abde02c8d9
William <titanbabrisa@gmail.com>
2024-05-01 17:56:54 -0400

    not gonna work!

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This module defines update_topics"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """This module updates mongo_collection based on name,topics"""
+
+    filters = {'name': name}
+    update = {'$set': {'topics': topics}}
+    mongo_collection.update(filters, update)
