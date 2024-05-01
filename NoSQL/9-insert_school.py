cd14c6cef67d459779bd9638818d9e12fdd6f45b
William <titanbabrisa@gmail.com>
2024-05-01 17:33:04 -0400

    mongo

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""This module defines insert_school"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """This module inserts documents from kwargs to collection"""
+
+    target_id = mongo_collection.insert(**kwargs)
+    return target_id
