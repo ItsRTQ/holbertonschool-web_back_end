fd068015413fa8320f71e96032489dc05e354158
William <titanbabrisa@gmail.com>
2024-05-01 17:20:48 -0400

    mogo stuff

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""This modules defines list_all"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """This method takes a mongo collection and returns a all documents"""
+
+    data = mongo_collection.find()
+    if data.count() == 0:
+        return []
+    else:
+        return data
