7b09ef3834c3d531dcba82d2bb69982ff97a0d96
William <titanbabrisa@gmail.com>
2024-05-01 18:20:33 -0400

    weeee

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""This module defines school_by_topic"""
+import pymongo
+
+def schools_by_topic(mongo_collection, topic):
+    """This method returns the list of school having a specific topic"""
+
+    data = mongo_collection.find(topic)
+    if data.count() == 0:
+        return []
+    else:
+        return data
