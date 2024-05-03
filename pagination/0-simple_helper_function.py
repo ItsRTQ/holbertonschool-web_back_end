caea37e2561d940a5e9c798b0fe070b5ffc2a4dc
William <titanbabrisa@gmail.com>
2024-05-03 17:20:08 -0400

    change end equation, not needed value

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,5 +6,5 @@ def index_range(page, page_size):
     """Returns tuple containing the beginning and end of pages base on index"""
 
     start = (page - 1) * page_size
-    end = start + page_size - 1
-    return start, end
+    end = start + page_size
+    return (start, end)
