85faabb69ba966d3f1462a751a062b7317e34c50
William <titanbabrisa@gmail.com>
2024-05-03 17:17:34 -0400

    fix attemtp

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,8 +5,6 @@
 def index_range(page, page_size):
     """Returns tuple containing the beginning and end of pages base on index"""
 
-    if page < 1 or page_size < 1:
-        return (0, 0)
     start = (page - 1) * page_size
     end = start + page_size - 1
-    return (start, end)
+    return start, end
