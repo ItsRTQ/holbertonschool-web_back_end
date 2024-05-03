4a4e50f2667d24378a0c132e1595f76590edf563
William <titanbabrisa@gmail.com>
2024-05-03 17:14:38 -0400

    task 0 index_range

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""This module defines index_range to be use else where"""
+
+
+def index_range(page, page_size):
+    """Returns tuple containing the beginning and end of pages base on index"""
+
+    start = (page - 1) * page_size
+    end = start + page_size - 1
+    return (start, end)
