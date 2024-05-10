654f2a465a4bd4a8bbf2d922663c374bd5eeec4f
William <titanbabrisa@gmail.com>
2024-05-10 01:52:45 -0400

    not work first

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -30,7 +30,7 @@ export default class Currency {
   }
 
   displayFullCurrency() {
-    const text = this._name + ' (' + this._code + ')';
+    const text = '${this._name} (${this._code})';
     return text;
   }
 }
