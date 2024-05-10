2a88218b4894f9a4d6a15de11bade742c119bdb9
William <titanbabrisa@gmail.com>
2024-05-10 15:34:35 -0400

    additions

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,1 +1,23 @@
-export default Building
+export default class Building {
+  /**
+  * 
+  * @param {number} sqft 
+  */
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw TypeError('Sqft must me a number');
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+  
+  set sqft(value) {
+    if (typeof value !== 'number') throw TypeError('Sqft must me a number');
+    this._sqft = value;
+  }
+  
+  evacuationWarningMessage() {
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
