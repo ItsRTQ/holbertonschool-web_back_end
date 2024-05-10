15a088242bbbf20ef1505e9e1c69db966849ca65
William <titanbabrisa@gmail.com>
2024-05-10 01:20:53 -0400

    fix lint and conditionals variable

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,22 +1,23 @@
 export default class HolbertonCourse {
   /**
-   * 
+   *
    * @param {String} name - name of the course
    * @param {Number} length - length of the course
    * @param {String[]} students - Students name
    */
   constructor(name, length, students) {
-   if (typeof value !== 'string') throw new TypeError('Name must be a string');
-   if (typeof value !== 'number') throw new TypeError('Length must be a number');
-   if (!Array.isArray(values)) throw new TypeError('Students must be a Array');
-   this._name = name;
-   this._length = length;
-   this._students = students;
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('Students must be a Array');
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
+
   set name(value) {
     if (typeof value !== 'string') throw new TypeError('Name must be a string');
     this._name = value;
@@ -25,6 +26,7 @@ export default class HolbertonCourse {
   get length() {
     return this._length;
   }
+
   set length(value) {
     if (typeof value !== 'number') throw new TypeError('Length must be a number');
     this._length = value;
@@ -33,6 +35,7 @@ export default class HolbertonCourse {
   get students() {
     return this._students;
   }
+
   set students(values) {
     if (!Array.isArray(values)) throw new TypeError('Students must be a Array');
     this._students = values;
