a8c02a64831a82d8ae1c96ce5186222fe98ac19c
William <titanbabrisa@gmail.com>
2024-05-10 01:34:51 -0400

    3 currecy task

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency {
+    /**
+     *
+     * @param {String} code - Code of currency
+     * @param {String} name - name of currency
+     */
+  constructor(code, name) {
+    if (typeof code !== 'string') throw TypeError('code must be string');
+    if (typeof name !== 'string') throw TypeError('name must be string');
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') throw TypeError('code must be string');
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw TypeError('name must be string');
+    this._name = value
+  }
+
+  displayFullCurrency() {
+    return '${this._name} (${this._code})'
+  }
+}
