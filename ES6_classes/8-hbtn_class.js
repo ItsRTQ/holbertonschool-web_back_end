beed12c5e1292863d21ce4bd9c5956bc6a64b8d6
William <titanbabrisa@gmail.com>
2024-05-10 16:02:15 -0400

    additions

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,38 @@
+export default class HolbertonClass {
+  /**
+  * @param {number} size - size of the class
+  * @param {string} location - location of the class
+  */
+  constructor(size, location) {
+    if (typeof size !== 'number' || typeof location !== 'string') {
+      throw new TypeError('Size must be a number and location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
