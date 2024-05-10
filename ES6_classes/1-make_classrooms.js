493928948e693209f9d9b4adc643db2da2e3bbd3
William <titanbabrisa@gmail.com>
2024-05-10 00:46:48 -0400

    hg

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -5,6 +5,6 @@ function initializeRooms() {
    * Creates function @see {initializeRooms}
    * Returns rooms array
    */
-  var rooms = [ClassRoom(19), ClassRoom(20), ClassRoom(34)]
-  return rooms
+  const rooms = [ClassRoom(19), ClassRoom(20), ClassRoom(34)];
+  return rooms;
 }
