2c5a0b876d90d66af2825a68f4fbbdadeeb2d17b
William <titanbabrisa@gmail.com>
2024-05-10 00:41:18 -0400

    gah

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from "./0-classroom.js";
+
+function initializeRooms() {
+  /**
+   * Creates function @see {initializeRooms}
+   * Returns rooms array
+   */
+  const rooms = [ClassRoom(19), ClassRoom(20), ClassRoom(34)]
+  return rooms
+}
--- ES6_classes/main.js
+++ ES6_classes/main.js
@@ -1,6 +1,3 @@
-import ClassRoom from "./0-classroom.js";
+import initializeRooms from './1-make_classrooms.js';
 
-test("Initialize an instance of ClassRoom set to 100", () => {
-  const room = new ClassRoom(100);
-  expect(room._maxStudentsSize).toBe(100);
-});
+console.log(initializeRooms());
