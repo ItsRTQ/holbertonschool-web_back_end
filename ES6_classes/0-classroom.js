547111d2244c98fb24a931d48ea8059c3e34b78e
William <titanbabrisa@gmail.com>
2024-05-09 23:49:59 -0400

    ll

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,4 +1,8 @@
 export default class ClassRoom {
+    /**
+    * Creates a new ClassRoom instance.
+    * @param {number} maxStudentsSize - The maximum number of students that can be accommodated in the classroom.
+    */
     constructor(maxStudentsSize) {
         this._maxStudentsSize = Number(maxStudentsSize);
     }
