b3c0ee626b62ac17e4e75d71dc0766385f585c29
William <titanbabrisa@gmail.com>
2024-05-10 00:13:38 -0400

    lk

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -3,7 +3,7 @@ export default class ClassRoom {
      * Creates a new @see {@link ClassRoom}
      * @param {Number} maxStudentsSize - Maximum number of students
      */
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
