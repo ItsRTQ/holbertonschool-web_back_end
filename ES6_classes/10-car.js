0c9ee8b0d4473de5eeb73c1081180eb3e3d87275
William <titanbabrisa@gmail.com>
2024-05-10 16:25:55 -0400

    additions

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,32 @@
+export default class Car {
+  /**
+   * This class represents a car
+   * @param {String} brand - brand of the car
+   * @param {String} motor - motor of the car
+   * @param {String} color - color of the car
+   */
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
+        throw TypeError('Brand, Motor, and color of car must be type string');
+    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
