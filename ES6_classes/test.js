4830ecc9e9a5d783d39b010e534b18e51011890e
William <titanbabrisa@gmail.com>
2024-05-09 23:40:50 -0400

    aggaa

--- ES6_classes/test.js
+++ ES6_classes/test.js
@@ -1,4 +1,6 @@
 import ClassRoom from "./0-classroom.js";
 
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
+test("Initialize an instance of ClassRoom set to 100", () => {
+  const room = new ClassRoom(100);
+  expect(room._maxStudentsSize).toBe(100);
+});
