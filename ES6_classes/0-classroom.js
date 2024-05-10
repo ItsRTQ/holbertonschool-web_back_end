4059992453753431e4094febfb25a71f17ceb4b4
William <titanbabrisa@gmail.com>
2024-05-10 00:22:04 -0400

    kjf

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,9 +1,9 @@
 export default class ClassRoom {
-    /**
-     * Implementing class named ClassRoom
-     * @param {Number} maxStudentsSize - Maximum number of students
-     */
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  /**
+   * Implementing class named ClassRoom
+   * @param {Number} maxStudentsSize - Maximum number of students
+   */
+  constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+  }
 }
