601a279885c445574d8fcf6f3135841955193b14
William <titanbabrisa@gmail.com>
2024-05-10 16:36:12 -0400

    added setters to see if thats the problem

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -18,12 +18,24 @@ export default class Car {
     return this._brand;
   }
 
+  set brand(value) {
+    this._brand = value;
+  }
+
   get motor() {
     return this._motor;
   }
 
+  set motor(value) {
+    this._motor = value;
+  }
+
   get color() {
     return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
   }
 
   cloneCar() {
