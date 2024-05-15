ab2d5cebe0efdd143ff7d31b072da0b03b02f8aa
William <titanbabrisa@gmail.com>
2024-05-14 23:56:06 -0400

    pushing the string literal

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,12 +1,11 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  const msg = 'Guardrail was processed';
   try {
     queue.push(mathFunction());
   } catch(err) {
     queue.push(String(err));
   } finally {
-    queue.push(msg);
+    queue.push('Guardrail was processed');
   }
   return queue;
 }
