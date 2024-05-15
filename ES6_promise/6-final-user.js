d7a43a8090b8e5ecfc21eef90bd35900bae3e9f0
William <titanbabrisa@gmail.com>
2024-05-14 23:04:04 -0400

    task 6 final user

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpResult = await signUpUser(firstName, lastName);
+  const uploadResult = await uploadPhoto(fileName);
+  const data = [{
+    status: signUpResult.status(),
+    value: signUpResult,
+  }, {
+    status: uploadResult.status(),
+    value: uploadResult,
+  }]
+}
